47d6c1362b95d0941e21d9a3391cec6883dc4971
Jean Carlos Berrocal Muñoz <berrocal13@gmail.com>
2018-05-07 16:35:15 -0600

    Crear modelo

---
+++ MoneyMakingWallet/MoneyMakingWallet/Model/APIDictionary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDMONEY.IO.Model
+{
+    public class APIDictionary
+    {
+
+    }
+}
---
+++ MoneyMakingWallet/MoneyMakingWallet/Model/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDMONEY.IO.Model
+{
+    public class UserModel : RealmObject
+    {
+        [PrimaryKey]
+        public long Id { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class UserRequest : BaseRequest
+    {
+
+    }
+}
