44e3fc4c5384d67ab8d6ab5fe6267ca8ddf6eb1b
Jean Carlos Berrocal Muñoz <berrocal13@gmail.com>
2018-05-31 20:20:58 -0600

    Login

--- MoneyMakingWallet/MoneyMakingWallet/Model/APIDictionary.cs
+++ MoneyMakingWallet/MoneyMakingWallet/Model/APIDictionary.cs
@@ -8,6 +8,7 @@ namespace IDMONEY.IO.Model
     public class APIDictionary
     {
         private const string Base = "http://jeancarlos.eastus2.cloudapp.azure.com/WebApiMoneyMakingWallet2/";
+        //private const string Base = "http://54.70.160.109/webapimoneymakingwallet/";
 
         #region Users
         public const string API_CreateUser = Base + "api/User/Create";
