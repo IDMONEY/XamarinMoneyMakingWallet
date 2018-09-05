b8061841449f46f7f6e324bb8ae25430a7dfff6a
Nelson Badilla <nelbadiaz@outlook.com>
2018-09-04 20:16:00 -0600

    Adding new methods to the Ensure

--- MoneyMakingWallet/MoneyMakingWallet/Core/Ensure.cs
+++ MoneyMakingWallet/MoneyMakingWallet/Core/Ensure.cs
@@ -15,5 +15,24 @@ namespace IDMONEY.IO
                 throw new ArgumentException(String.Format("{0} cannot be null or empty", nameof(parameter)));
             }
         }
+
+        [DebuggerStepThrough]
+        public static void IsNotNegativeOrZero(long parameter)
+        {
+            if (parameter <= 0)
+            {
+                throw new ArgumentException(String.Format("{0} cannot be negative or equals to zero", nameof(parameter)));
+            }
+        }
+
+        [DebuggerStepThrough]
+        public static void IsNotNegative(long parameter)
+        {
+            if (parameter < 0)
+            {
+                throw new ArgumentException(String.Format("{0} cannot be negative", nameof(parameter)));
+            }
+        }
+
     }
 }
